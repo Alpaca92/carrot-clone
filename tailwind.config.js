e2b4f6ee988ae005e9e5f6ee2f04b19c605a4819
ayaan <ayaan@t-win.kr>
2023-04-25 17:55:01 +0900

    #5.2

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx}",
-    "./src/components/**/*.{js,ts,jsx,tsx}",
-    "./src/app/**/*.{js,ts,jsx,tsx}",
+    './src/pages/**/*.{js,ts,jsx,tsx}',
+    './src/components/**/*.{js,ts,jsx,tsx}',
+    './src/app/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
     extend: {},
   },
-  darkMode: "class",
-  plugins: [],
+  darkMode: 'class',
+  plugins: [require('@tailwindcss/forms')],
 };
