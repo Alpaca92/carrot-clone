b658539df8985769e2b7f6b3e5344c11b4ec3ca4
Ayaan(home) <zxcvbnm5288@gmail.com>
2023-05-10 11:52:14 +0900

    #9.1 part 1

--- src/pages/api/users/enter.tsx
+++ src/pages/api/users/enter.tsx
@@ -1,8 +1,58 @@
+import client from '@/libs/client/client';
 import withHandler from '@/libs/server/withHandler';
 import { NextApiRequest, NextApiResponse } from 'next';
 
 async function handler(req: NextApiRequest, res: NextApiResponse) {
-  console.log(req.body);
+  const { phone, email } = req.body;
+
+  if (email) {
+    let user = await client.user.findUnique({
+      where: {
+        email,
+      },
+    });
+
+    if (user) {
+      console.log('user found');
+    }
+
+    if (!user) {
+      console.log('did not found and will create a new user');
+      user = await client.user.create({
+        data: {
+          name: 'Anonymous',
+          email,
+        },
+      });
+    }
+
+    console.log(user);
+  }
+
+  if (phone) {
+    let user = await client.user.findUnique({
+      where: {
+        phone: +phone,
+      },
+    });
+
+    if (user) {
+      console.log('user found');
+    }
+
+    if (!user) {
+      console.log('did not found and will create a new user');
+      user = await client.user.create({
+        data: {
+          name: 'Anonymous',
+          phone: +phone,
+        },
+      });
+    }
+
+    console.log(user);
+  }
+
   return res.status(200).end();
 }
 
