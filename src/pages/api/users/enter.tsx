ee005d40a1e535e1b036e29bae4c4154259aa025
Ayaan(home) <zxcvbnm5288@gmail.com>
2023-05-14 19:51:35 +0900

    #9.5

--- src/pages/api/users/enter.tsx
+++ src/pages/api/users/enter.tsx
@@ -1,8 +1,10 @@
 import client from '@/libs/client/client';
+import mail from '@sendgrid/mail';
 import withHandler, { ResponseType } from '@/libs/server/withHandler';
 import { NextApiRequest, NextApiResponse } from 'next';
 import twilio from 'twilio';
 
+mail.setApiKey(process.env.SENDGRID_KEY!);
 const twilioClient = twilio(process.env.TWILIO_SID, process.env.TWILIO_TOKEN);
 
 async function handler(
@@ -41,6 +43,16 @@ async function handler(
     });
 
     console.log(message);
+  } else if (email) {
+    const email = await mail.send({
+      from: 'be.minimal.o.o9@gmail.com',
+      to: 'zxcvbnm5288@naver.com',
+      subject: 'Your carrot market verification Email',
+      text: `your token is ${payload}`,
+      html: `<strong>your token is ${payload}</strong>`,
+    });
+
+    console.log(email);
   }
 
   return res.json({ ok: true });
