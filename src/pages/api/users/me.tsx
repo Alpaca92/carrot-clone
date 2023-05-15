8787b460966c077bcf7892a6bffec99f916aedd7
Ayaan(home) <zxcvbnm5288@gmail.com>
2023-05-15 21:49:15 +0900

    #9.8

--- src/pages/api/users/me.tsx
+++ src/pages/api/users/me.tsx
@@ -16,25 +16,21 @@ async function handler(
   req: NextApiRequest,
   res: NextApiResponse<ResponseType>
 ) {
-  const { token } = req.body;
-  const exists = await client.token.findUnique({
+  console.log(req.session.user);
+  const { id } = req.session.user;
+  const profile = await client.user.findUnique({
     where: {
-      payload: token,
+      id,
     },
   });
 
-  if (!exists) return res.status(404).end();
-
-  req.session.user = {
-    id: exists.userId,
-  };
-
-  await req.session.save();
-
-  res.status(200).end();
+  res.json({
+    ok: true,
+    profile,
+  });
 }
 
-export default withIronSessionApiRoute(withHandler('POST', handler), {
+export default withIronSessionApiRoute(withHandler('GET', handler), {
   cookieName: 'carrot-session',
   password: '129if,-f0i1w-di1w0-di192u89012xue9120012emu190u90eu129e129x91uh',
 });
